435597574e414f14bdeb69d27bade5c87036efcd
lucas pucci <lucas.serge.pucci@gmail.com>
2022-12-12 11:02:32 +0100

    connexion

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,66 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require("../models/connection");
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+const bcrypt = require("bcrypt");
+const uid2 = require("uid2");
+
+//route d'inscription
+router.post("/signup", (req, res) => {
+  if (!checkBody(req.body, ["firstName", "username", "email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  // vérifier si l'utilisateur n'a pas déjà été enregistré
+  User.findOne({
+    email: { $regex: new RegExp(req.body.email, "i") },
+  }).then((data) => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        firstName: req.body.firstName,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: hash,
+        token: uid2(32),
+        canBookmark: true,
+      });
+
+      newUser.save().then((newDoc) => {
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+      //L'utilisateur existe déjà dans la base de données
+      res.json({ result: false, error: "User already exists" });
+    }
+  });
+});
+
+//route de connexion
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  User.findOne({
+    username: { $regex: new RegExp(req.body.username, "i") },
+  }).then((data) => {
+    if (bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({
+        result: true,
+        token: data.token,
+        username: data.username,
+        firstName: data.firstName,
+      });
+    } else {
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
 });
 
 module.exports = router;
