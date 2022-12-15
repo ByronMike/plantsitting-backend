b537f255deead38dc9f4fc498a5f9d8aa72c5ad2
lucas pucci <lucas.serge.pucci@gmail.com>
2022-12-15 17:31:09 +0100

    chat

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ require("./models/connection");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-<<<<<<< HEAD
-=======
 var sittersRouter = require("./routes/sitters");
->>>>>>> ecd4d461a147b000f3bb8a7b799a40136472dc88
 var assessmentRouter = require("./routes/assessment");
 
 var app = express();
@@ -26,10 +23,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-<<<<<<< HEAD
-app.use("/assessment", assessmentRouter);
-=======
 app.use("/sitters", sittersRouter);
->>>>>>> ecd4d461a147b000f3bb8a7b799a40136472dc88
-
+app.use("/assessment", assessmentRouter);
 module.exports = app;
